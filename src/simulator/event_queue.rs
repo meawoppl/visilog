52bee8fb0e1b15f980b0116183e5802964d545a4
meawoppl <meawoppl@gmail.com>
2025-02-23 09:43:47 -0800

    Add the event queue  + tests

--- src/simulator/event_queue.rs
+++ src/simulator/event_queue.rs
@@ -8,23 +8,39 @@ enum ExecutionCursor {
 
 pub struct EventQueue {
     // The next exection time, and the cursor that execution will be resumed at
-    cursors: Vec<(i64, ExecutionCursor)>,
+    cursors: Vec<ExecutionCursor>,
+    times: Vec<i64>,
 }
 
 impl EventQueue {
     pub fn new() -> Self {
         EventQueue {
             cursors: Vec::new(),
+            times: Vec::new(),
         }
     }
 
     pub fn insert(&mut self, time: i64, cursor: ExecutionCursor) {
-        // TODO(meawoppl) - test insert stability
         let position = self
-            .cursors
-            .binary_search_by_key(&time, |&(t, _)| t)
-            .unwrap_or_else(|e| e);
-        self.cursors.insert(position, (time, cursor));
+            .times
+            .binary_search(&time);
+
+        let position = match position {
+            // Found a match, so we have to search for the last match
+            Ok(position) => {
+                let mut last_match = position;
+                while last_match < self.times.len() && self.times[last_match] == time {
+                    last_match += 1;
+                }
+                last_match
+            },
+            // No match, so insert is default safe
+            Err(position) => position,
+        };
+
+        // Insert the time and cursor
+        self.cursors.insert(position,  cursor);
+        self.times.insert(position, time);
     }
 }
 
@@ -48,8 +64,8 @@ mod tests {
         queue.insert(5, ExecutionCursor::AlwaysCursor((1, always_block)));
 
         assert_eq!(queue.cursors.len(), 2);
-        assert_eq!(queue.cursors[0].0, 5);
-        assert_eq!(queue.cursors[1].0, 10);
+        assert_eq!(queue.times[0], 5);
+        assert_eq!(queue.times[1], 10);
     }
 
     #[test]
@@ -62,7 +78,12 @@ mod tests {
         queue.insert(10, ExecutionCursor::InitialCursor((1, initial_block2)));
 
         assert_eq!(queue.cursors.len(), 2);
-        assert_eq!(queue.cursors[0].0, 10);
-        assert_eq!(queue.cursors[1].0, 10);
+        assert_eq!(queue.times[0], 10);
+        assert_eq!(queue.times[1], 10);
     }
+
+    fn test_event_queue_insert_stable() {
+
+    }
+
 }
