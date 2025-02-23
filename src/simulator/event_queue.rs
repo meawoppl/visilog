caa9ab41128b3198836c245a129607a294a64e77
MattyG <meawoppl@gmail.com>
2025-02-23 09:59:31 -0800

    Add the event queue  + tests (#45)
    * Add the event queue  + tests
    
    * Harder test

--- src/simulator/event_queue.rs
+++ src/simulator/event_queue.rs
@@ -8,23 +8,37 @@ enum ExecutionCursor {
 
 pub struct EventQueue {
     // The next exection time, and the cursor that execution will be resumed at
-    cursors: Vec<(i64, ExecutionCursor)>,
+    cursors: Vec<ExecutionCursor>,
+    times: Vec<i64>,
 }
 
 impl EventQueue {
     pub fn new() -> Self {
         EventQueue {
             cursors: Vec::new(),
+            times: Vec::new(),
         }
     }
 
     pub fn insert(&mut self, time: i64, cursor: ExecutionCursor) {
-        // TODO(meawoppl) - test insert stability
-        let position = self
-            .cursors
-            .binary_search_by_key(&time, |&(t, _)| t)
-            .unwrap_or_else(|e| e);
-        self.cursors.insert(position, (time, cursor));
+        let position = self.times.binary_search(&time);
+
+        let position = match position {
+            // Found a match, so we have to search for the last match
+            Ok(position) => {
+                let mut last_match = position;
+                while last_match < self.times.len() && self.times[last_match] == time {
+                    last_match += 1;
+                }
+                last_match
+            }
+            // No match, so insert is default safe
+            Err(position) => position,
+        };
+
+        // Insert the time and cursor
+        self.cursors.insert(position, cursor);
+        self.times.insert(position, time);
     }
 }
 
@@ -48,21 +62,45 @@ mod tests {
         queue.insert(5, ExecutionCursor::AlwaysCursor((1, always_block)));
 
         assert_eq!(queue.cursors.len(), 2);
-        assert_eq!(queue.cursors[0].0, 5);
-        assert_eq!(queue.cursors[1].0, 10);
+        assert_eq!(queue.times[0], 5);
+        assert_eq!(queue.times[1], 10);
     }
 
     #[test]
     fn test_event_queue_insert_same_time() {
         let mut queue = EventQueue::new();
-        let initial_block1 = InitialBlock::new(vec![]);
-        let initial_block2 = InitialBlock::new(vec![]);
 
-        queue.insert(10, ExecutionCursor::InitialCursor((0, initial_block1)));
-        queue.insert(10, ExecutionCursor::InitialCursor((1, initial_block2)));
+        queue.insert(
+            10,
+            ExecutionCursor::InitialCursor((0, InitialBlock::new(vec![]))),
+        );
+        queue.insert(
+            10,
+            ExecutionCursor::InitialCursor((0, InitialBlock::new(vec![]))),
+        );
 
-        assert_eq!(queue.cursors.len(), 2);
-        assert_eq!(queue.cursors[0].0, 10);
-        assert_eq!(queue.cursors[1].0, 10);
+        for x in 0..10 {
+            queue.insert(
+                10,
+                ExecutionCursor::AlwaysCursor((0, AlwaysBlock::new(vec![], vec![]))),
+            );
+
+            match queue.cursors.last().unwrap() {
+                ExecutionCursor::AlwaysCursor((_, _)) => {}
+                _ => panic!("Expected AlwaysBlock"),
+            }
+
+            queue.insert(
+                10,
+                ExecutionCursor::InitialCursor((0, InitialBlock::new(vec![]))),
+            );
+
+            match queue.cursors.last().unwrap() {
+                ExecutionCursor::InitialCursor((_)) => {}
+                _ => panic!("Expected InitialBlock"),
+            }
+        }
     }
+
+    fn test_event_queue_insert_stable() {}
 }
